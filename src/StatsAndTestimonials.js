03d43fec263b41e0ba724d257595c4a26e16cffa
Divya Yash <87994860+Valium-stash-on-Floor13@users.noreply.github.com>
2022-05-25 17:20:51 +0530

    Update StatsAndTestimonials.js

--- src/StatsAndTestimonials.js
+++ src/StatsAndTestimonials.js
@@ -40,7 +40,7 @@ const StatsAndTestimonials = () => {
    </div>
 
  </div>
- <h3 className='text-white text-xl text-center mx-auto max-w-5xl'> See what our cutsomers have to say</h3>
+ <h3 className='text-white text-xl text-center mx-auto max-w-5xl'> See what our customers have to say</h3>
 
 </div>
 <svg className="z-0"  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#1C1943" fill-opacity="1" d="M0,96L48,128C96,160,192,224,288,229.3C384,235,480,181,576,144C672,107,768,85,864,112C960,139,1056,213,1152,224C1248,235,1344,181,1392,154.7L1440,128L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
